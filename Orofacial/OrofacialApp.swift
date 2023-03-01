a1ea0903a18da70d9e95c7b2cdda96854907972c
emshlee <emshlee01@gmail.com>
2023-03-01 18:34:51 -0500

    Initial Commit

---
+++ Orofacial/OrofacialApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrofacialApp.swift
+//  Orofacial
+//
+//  Created by Emma Sihyun Lee on 2023-03-01.
+//
+
+import SwiftUI
+
+@main
+struct OrofacialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
