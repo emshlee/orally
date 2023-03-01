3cde4494d0c3e8ad1ab2cb42b1aa39f49f0f5af2
emshlee <emshlee01@gmail.com>
2023-03-01 18:34:51 -0500

    Initial Commit

---
+++ Orofacial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Orofacial
+//
+//  Created by Emma Sihyun Lee on 2023-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
