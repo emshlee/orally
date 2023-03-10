3362fffd69cfd84db4624fa15cbdb1ae66840388
Taewon Hwang <taewonhwang7@gmail.com>
2023-03-10 18:36:42 -0500

    made logo screen using custom font

---
+++ Orofacial/Assets.swift
@@ -0,0 +1,8 @@
+//
+//  Assets.swift
+//  Orofacial
+//
+//  Created by Emma Sihyun Lee on 2023-03-10.
+//
+
+import Foundation
--- Orofacial/ContentView.swift
+++ Orofacial/ContentView.swift
@@ -9,6 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
+            Color(red: 0.68, green: 0.89, blue: 1.00)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Orofacial")
+                    .font(Font.custom("KronaOne-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("Content??")
+            }
+        }
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
