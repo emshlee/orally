fafb5ecfc568c239b03057a4a0073f242bed09e7
emshlee <emshlee01@gmail.com>
2023-03-21 01:25:37 -0400

    added onboarding page and login page

--- Orofacial/Screens/LoginView.swift
+++ Orofacial/Screens/LoginView.swift
@@ -8,10 +8,62 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView{
+                VStack (spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                    if !isLoginMode {
+                        Button {
+                            
+                        } label:  {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .padding(12)
+                        .background(Color.white)
+                    SecureField("Password", text: $password)
+                        .padding(12)
+                        .background(Color.white)
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 8)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                        
+                    }
+                }
+                .padding()
+                .backgroundStyle(Color("ColorBlueLight"))
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            
+        }
     }
 }
+                
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
