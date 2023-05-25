d632134952b16aaa2c9207ef28c0c2f351d661e5
emshlee <emshlee01@gmail.com>
2023-05-24 22:25:49 -0400

    Added DentalInfoButton and DentalInfoPage

---
+++ Orofacial/Screens/Dental info/DentalInfoPage.swift
@@ -0,0 +1,20 @@
+//
+//  DentalInfoPage.swift
+//  Orofacial
+//
+//  Created by Emma Sihyun Lee on 2023-05-24.
+//
+
+import SwiftUI
+
+struct DentalInfoPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DentalInfoPage_Previews: PreviewProvider {
+    static var previews: some View {
+        DentalInfoPage()
+    }
+}
