cfe09852227211ce0cbe51578104af17269cfaa6
Taewon Hwang <taewonhwang7@gmail.com>
2023-06-05 21:25:55 -0400

    test

--- Orofacial/Screens/Profile/ProfileView.swift
+++ Orofacial/Screens/Profile/ProfileView.swift
@@ -56,12 +56,73 @@ struct ProfileView: View {
                             } label: {
                                 PrimaryButton(text: "Start self assessment")
                             }
+                            checkup()
                         }
                     }
                 }
             }
         }
         
+    }
+}
+struct checkup: View {
+    @State var checkUpDate = Date()
+    @State var nextCheckUpDate = Date()
+
+    var body: some View {
+        VStack(alignment: .center) {
+            Image("calendarlogo2")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30)
+                .padding(.top, 20)
+            
+            Text("Check Up Reminder")
+                .bold()
+                .font(.title)
+                .background(Color("BackgroundColor"))
+                .foregroundColor(Color("AccentColor"))
+                .cornerRadius(60.0)
+            
+            Text("Your Last Assessment Date")
+                .bold()
+                .padding()
+                .background(Color("BackgroundColor"))
+                .cornerRadius(60.0)
+            
+            VStack {
+                DatePicker("Last Assessment", selection: $checkUpDate, in: ...Date(), displayedComponents: .date)
+                    .font(.system(size: 20))
+                    .frame(width: 300, height: 40)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.gray, lineWidth: 2)
+                    )
+            }
+            
+            Text("Your Next Assessment Date")
+                .bold()
+                .padding()
+            
+            Text(nextCheckUpDate, format: .dateTime.day().month().year())
+                .padding()
+            
+        }
+        .padding()
+        .padding(.top)
+        .padding(.bottom, 50)
+        .background(Color("BackgroundColor"))
+        .cornerRadius(60.0)
+        .onAppear {
+            nextDate()
+        }
+    }
+    
+    func nextDate() {
+        let calendar = Calendar.current
+        let nextDate = calendar.date(byAdding: .day, value: 180, to: checkUpDate)
+        nextCheckUpDate = nextDate ?? Date()
     }
 }
 
